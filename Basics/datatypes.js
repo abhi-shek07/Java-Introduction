0d6961f2a3844b30a6ee33a05a491c27b43b2297
abhi-shek07 <150690125+abhi-shek07@users.noreply.github.com>
2024-08-28 20:26:32 +0000

    datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict";  //treat all JS code as newer version
+
+//alert(3+5) //we are using nodejs. not browser..as in chrome browser their some functionality or syntax changes
+ 
+console.log(3+9);
+console.log("Abhishek");  //code redability should be high
+
+let name="sahil";
+let age=123;
+let isLoggedIn= false;
+console.table([name,age,isLoggedIn])
+let namo = "abhi";
+console.log(typeof name);
+//data types//
+//number => 2 to power 53
+//bigint(for ver big numbers as used in stocks)
+//string =>""(write in double or single code)
+//boolean =>true/false
+//null =>standalone value(defined empty value)
+//undefined => value is not aSSIGNED
+//symbol => to shoe unique componentys/symbols
+
+//object
+
---
+++ Basics/variable.js
@@ -0,0 +1,20 @@
+const accountId = 12345
+let accountEmail = "abhish43437@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+let accountboard;
+// accountId = 2
+
+console.log(accountId);
+
+accountEmail = "dhh@3"
+accountPassword = "2345467"
+accountCity = "bihar"
+
+
+/*
+we will not use VAR keyword because it
+has issues withblock scope and functional scope
+*/
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountboard])
