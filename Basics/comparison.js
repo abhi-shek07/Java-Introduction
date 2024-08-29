466a354f46e8a8599801b33aa179461da9e5df21
abhi-shek07 <150690125+abhi-shek07@users.noreply.github.com>
2024-08-29 15:58:40 +0000

    compare with logic not simply compare two different datatype

---
+++ Basics/comparison.js
@@ -0,0 +1,15 @@
+console.log(2>1);
+console.log(3<4);
+console.log(5>8);
+console.log(2==1);
+console.log(2!=1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined<0);
+
+console.log("2" === 2);
