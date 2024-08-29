fdf3d060d8bf7df9a5af674b520e5517e3fa8ec6
abhi-shek07 <150690125+abhi-shek07@users.noreply.github.com>
2024-08-29 21:32:54 +0000

    Two types of memoey:-Heaps & stacks

--- Basics/conversionOperation.js
+++ Basics/conversionOperation.js
@@ -20,3 +20,44 @@ let somenumber= 34;
 let stringnumber= String(somenumber);
 console.log(stringnumber);
 console.log(typeof stringnumber);
+
+
+//*****************************************Operations****************************************
+
+let value=3;
+let negvalue=-value;
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2*3);
+console.log(34/23);
+console.log(2**4);
+console.log(2**8);
+console.log(43/234);
+console.log(3**2);
+console.log(323%323);
+console.log(23%12);
+
+let str1="hello";
+let str2=" Abhishek";
+let str3=str1+str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1+"2");
+console.log(1+2+"45")
+console.log((3+4) *4%5);
+
+console.log(true);
+console.log(+true);
+console.log(false);
+console.log(+false);
+
+let num1,num2,num3,num4;
+num1 = num2 = num3 = num4 = 2+2;
+console.log(num4);
+
+let gamecounnter=100;
+gamecounnter++;
+++gamecounnter;
+console.log(gamecounnter);
---
+++ Basics/memory.js
@@ -0,0 +1,15 @@
+
+let a="abhishek43437";
+let b=a;
+a="chaiaurcode";
+console.log(a);
+console.log(b);
+
+let user1={
+    email:"abhi4343",
+    upi:"545@45",
+}
+let user2=user1;
+user2.email="alok4343";
+console.log(user1.email);
+console.log(user2.email);
