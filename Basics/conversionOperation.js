cac5eae2fede468345ba4b34e89a9161a82ba66e
abhi-shek07 <150690125+abhi-shek07@users.noreply.github.com>
2024-08-29 07:45:09 +0000

    conversion of datatypes

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,22 @@
+const num=undefined;
+
+console.log(typeof num);
+console.log(typeof(num));
+
+let valueInNumber = Number(num);
+// /console.log(typeof valueInNumber);
+
+let isloggedin = "Abhishek";
+
+let booleanisloggedin = Boolean(isloggedin);
+console.log(booleanisloggedin);
+
+// 1=>true;
+//0=>false
+//""=>false
+//"Abhiskek"=>true
+
+let somenumber= 34;
+let stringnumber= String(somenumber);
+console.log(stringnumber);
+console.log(typeof stringnumber);
