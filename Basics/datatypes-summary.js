c230cf44fa403a094d4e446b85e02afe0539d026
abhi-shek07 <150690125+abhi-shek07@users.noreply.github.com>
2024-08-29 20:33:50 +0000

    data type summary

---
+++ Basics/datatypes-summary.js
@@ -0,0 +1,21 @@
+const id=Symbol('123');
+const anotherid=Symbol('123');
+console.log(id === anotherid);
+
+const bigint=2353465474563423243464534n;
+//console.log(bignumber);
+
+const heros= ["shaktiman","naagraj", "doga"];
+let myobj={
+name:"Abhishek",
+age:"20"
+}
+
+const myself={
+    name:"abhishek",
+    school:"SJS",
+    age:67,
+}
+console.log(heros);
+console.log(myobj);
+console.log(myself);
